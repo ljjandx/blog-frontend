46d01fe628f0a5e8d6777f346f900014cb53de7c
lv_jing <lv_jing@yeah.net>
2021-04-09 18:11:11 +0800

    init

---
+++ gridsome.config.js
@@ -0,0 +1,49 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'gridsome',
+  siteDescription: 'gridsome',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'BlogPost',
+        path: './content/blog/**/*.md',
+      }
+    },
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: process.env.GRIDSOME_API_URL,
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['post', 'tag'],
+        // typeName: 'Strapi',
+        singleTypes: ['general'],
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // loginData: {
+        //   identifier: '',
+        //   password: ''
+        // }
+      }
+    }
+  ],
+  templates: {
+    StrapiPost: [ // 集合的名字，对应 plugins 中的 typeName + contentTypes，可以更改
+      {
+        path: '/post/:id',
+        component: './src/templates/Post.vue'
+      }
+    ],
+    StrapiTag: [
+      {
+        path: '/tag/:id',
+        component: './src/templates/Tag.vue'
+      }
+    ]
+  }
+}
---
+++ src/templates/Post.vue
@@ -0,0 +1,61 @@
+<template>
+  <Layout>
+    <!-- Page Header-->
+    <header class="masthead" style="background-image: url('assets/img/post-bg.jpg')">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto">
+                    <div class="post-heading">
+                        <h1>{{ $page.post.title }}</h1>
+                        <span class="meta">
+                            {{ $page.post.created_at }}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Post Content-->
+    <article>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-10 mx-auto" v-html="mdToHtml($page.post.content)"></div>
+            </div>
+        </div>
+    </article>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID) {
+  post: strapiPost (id: $id) {
+    id
+    title
+    content
+    created_at
+    tags {
+      id
+      title
+    }
+  }
+}
+</page-query>
+
+<script>
+import MarkdownIt from 'markdown-it'
+const md = new MarkdownIt()
+
+export default {
+  name: 'PostPage',
+  methods: {
+    mdToHtml (markdown) {
+      return md.render(markdown)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
